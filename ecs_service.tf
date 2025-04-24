7d376a4deec30bb1089c4d65fa95d7c9933dab83
Ramesh Shirwal <g24aito37@iitj.ac.in>
2025-04-24 15:23:06 +0530

    newly added

--- ecs_service.tf
+++ ecs_service.tf
@@ -8,9 +8,11 @@ resource "aws_ecs_service" "drupal_service" {
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.drupal.arn
   desired_count   = 1
-  launch_type     = "EC2"
+  launch_type     = "FARGATE"
+
   network_configuration {
     subnets          = [aws_subnet.public.id]
     assign_public_ip = true
+    security_groups  = [aws_security_group.ecs_sg.id]
   }
 }
